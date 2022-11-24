7c8166b78bdc2c50b77f5b1703a52e9d7518c30d
josebarrios99 <estaravia@hotmail.com>
2022-11-24 01:52:11 -0500

    productos

--- pronia/assets/js/bookStore.js
+++ pronia/assets/js/bookStore.js
@@ -20,15 +20,43 @@ const renderBooksIndex = () => {
     allBooksHtml = '';
 
     booksList.forEach(currentBook => {
+        let peticio = `<div class="product-item">
+        <div class="product-img">
+            <a href="shop.html">
+                <img class="primary-img" src="https://drive.google.com/uc?id=1Ph_d2lvpW5ozJ13ZTCZTRT0LHU1JGcTP" alt="Product Images">
+                <img class="secondary-img" src="assets/images/product/medium-size/L72.png" alt="Product Images">
+            </a>
+            <div class="product-add-action">
+                <ul>
+                    <li>
+                        <a href="wishlist.html" data-tippy="Add to wishlist" data-tippy-inertia="true" data-tippy-animation="shift-away" data-tippy-delay="50" data-tippy-arrow="true" data-tippy-theme="sharpborder">
+                            <i class="pe-7s-like"></i>
+                        </a>
+                    </li>
+                    <li class="quuickview-btn" data-bs-toggle="modal" data-bs-target="#quickModal">
+                        <a href="#" data-tippy="Quickview" data-tippy-inertia="true" data-tippy-animation="shift-away" data-tippy-delay="50" data-tippy-arrow="true" data-tippy-theme="sharpborder">
+                            <i class="pe-7s-look"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="cart.html" data-tippy="Add to cart" data-tippy-inertia="true" data-tippy-animation="shift-away" data-tippy-delay="50" data-tippy-arrow="true" data-tippy-theme="sharpborder">
+                            <i class="pe-7s-cart"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="product-content">
+            <a class="product-name" href="shop.html">`+ currentBook.name + `</a>
+            <a class="product-name2" href="shop.html">Autor</a>
+            <div class="price-box pb-1">
+                <span class="new-price">45.000 COP</span>
+            </div>
 
-        let bookDivHtml = '<div>' + currentBook.name + '</div>';
-        allBooksHtml += bookDivHtml;
-
-        let sakdhajksdh = `
-            <div>${currentBook.name}</div>
-        
-        
-        `;
+        </div>
+    </div>`;
+    
+        allBooksHtml += peticio;
 
 
     });
